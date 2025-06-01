641fd62723ddb4ee7c1a069dfca3b315d004d8a2
Shaharyar-WebDev <shery.codes@gmail.com>
2025-06-01 12:45:47 +0500

    artisan route for migrations and other operations.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,4 +10,21 @@ Route::get('/', function () {
 // 
 Route::get('/home', function () {
     return "HEY";
+});
+
+Route::get('/command/{command}', function($command){
+
+    $not_allowed = ['down'];
+
+    if(in_array($command, $not_allowed)){
+        abort(403);
+    }
+
+    Artisan::call($command);
+
+    $otuput = Artisan::output();
+
+    return nl2br($otuput);
+
+    
 });
